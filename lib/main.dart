2dc071164f6abcae3e83c2176ebbfd1c3e9560a6
Emanuel Lamba <emanuellamba13@gmail.com>
2023-10-02 11:47:40 +0300

    feat: make screen portrait only

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 import 'src/app.dart';
@@ -20,6 +21,8 @@ void main() async {
       print(e);
     }
   }
+
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   final authContainer = ProviderContainer();
   await authContainer.read(authControllerProvider).initState();

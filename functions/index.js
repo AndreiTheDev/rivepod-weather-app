34159dcaf0ff178afc0f4b8aebfa26429f658cf1
AndreiTheDev <andreithedevwork@gmail.com>
2024-02-09 18:32:19 +0200

    feat: reinitialize flutter for ios and generated icons for app build

--- functions/index.js
+++ functions/index.js
@@ -4,62 +4,60 @@ const functions = require("firebase-functions");
 
 admin.initializeApp();
 
-exports.addFriend = functions
-  .runWith({ enforceAppCheck: true })
-  .region("europe-west1")
-  .https.onCall((request) => {
-    logger.info(request);
-    const { currentUser, friend } = request;
-    logger.info(currentUser);
-    logger.info(friend);
-    const defaultFirestore = admin.firestore();
-    var batch = defaultFirestore.batch();
-    var userFriendsDoc = defaultFirestore
-      .collection("users")
-      .doc(currentUser.uid)
-      .collection("user_friends")
-      .doc(friend.uid);
-    batch.set(userFriendsDoc, friend);
-    var friendRequestsDoc = defaultFirestore
-      .collection("users")
-      .doc(friend.uid)
-      .collection("user_friends_requests")
-      .doc(currentUser.uid);
-    batch.set(friendRequestsDoc, currentUser);
-    batch.commit();
+// .runWith({ enforceAppCheck: true })
+exports.addFriend = functions.region("europe-west1").https.onCall((request) => {
+  logger.info(request);
+  const { currentUser, friend } = request;
+  logger.info(currentUser);
+  logger.info(friend);
+  const defaultFirestore = admin.firestore();
+  var batch = defaultFirestore.batch();
+  var userFriendsDoc = defaultFirestore
+    .collection("users")
+    .doc(currentUser.uid)
+    .collection("user_friends")
+    .doc(friend.uid);
+  batch.set(userFriendsDoc, friend);
+  var friendRequestsDoc = defaultFirestore
+    .collection("users")
+    .doc(friend.uid)
+    .collection("user_friends_requests")
+    .doc(currentUser.uid);
+  batch.set(friendRequestsDoc, currentUser);
+  batch.commit();
 
-    const receiver = admin
-      .firestore()
-      .collection("users")
-      .doc(friend.uid)
-      .get()
-      .then((response) => {
-        const payload = {
-          token: response.data().fcmToken,
-          notification: {
-            title: `${currentUser.username} sent you a friend request`,
-            body: "Don't let your new friend wait. Accept his request now.",
-          },
-          data: {
-            body: "Don't let your new friend wait. Accept his request now.",
-          },
-        };
-        admin
-          .messaging()
-          .send(payload)
-          .then((response) => {
-            logger.info("sent message");
-          })
-          .catch((error) => {
-            logger.error(error);
-          });
-      });
+  const receiver = admin
+    .firestore()
+    .collection("users")
+    .doc(friend.uid)
+    .get()
+    .then((response) => {
+      const payload = {
+        token: response.data().fcmToken,
+        notification: {
+          title: `${currentUser.username} sent you a friend request`,
+          body: "Don't let your new friend wait. Accept his request now.",
+        },
+        data: {
+          body: "Don't let your new friend wait. Accept his request now.",
+        },
+      };
+      admin
+        .messaging()
+        .send(payload)
+        .then((response) => {
+          logger.info("sent message");
+        })
+        .catch((error) => {
+          logger.error(error);
+        });
+    });
 
-    return;
-  });
+  return;
+});
 
+// .runWith({ enforceAppCheck: true })
 exports.deleteFriend = functions
-  .runWith({ enforceAppCheck: true })
   .region("europe-west1")
   .https.onCall((request) => {
     logger.info(request);
@@ -91,8 +89,8 @@ exports.deleteFriend = functions
     return;
   });
 
+// .runWith({ enforceAppCheck: true })
 exports.rejectFriend = functions
-  .runWith({ enforceAppCheck: true })
   .region("europe-west1")
   .https.onCall((request) => {
     logger.info(request);

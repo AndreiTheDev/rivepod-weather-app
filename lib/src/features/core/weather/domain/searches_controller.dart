67b876f2cab8d97f1fb6aaa4de6cc358f3f8d108
AndreiTheDev <andreithedevwork@gmail.com>
2024-04-23 20:00:01 +0300

    feat: implement new search controller so the loading state from stream will always be preserved

--- lib/src/features/core/weather/domain/searches_controller.dart
+++ lib/src/features/core/weather/domain/searches_controller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../services/weather_service.dart';
@@ -15,5 +16,49 @@ Stream<List<SearchModel>> searches(final SearchesRef ref) async* {
         .map((final element) => SearchModel.fromData(element.data()))
         .toList();
     yield allSearches;
+  }
+}
+
+class SearchesState {
+  final List<SearchModel> searchesList;
+  DocumentSnapshot? lastDocument;
+
+  SearchesState({required this.searchesList, this.lastDocument});
+
+  SearchesState copyWith(
+    final List<SearchModel>? searchesList,
+    final DocumentSnapshot? lastDocument,
+  ) {
+    return SearchesState(
+      searchesList: searchesList ?? this.searchesList,
+      lastDocument: lastDocument ?? this.lastDocument,
+    );
+  }
+}
+
+@Riverpod(keepAlive: true)
+class SearchesController extends _$SearchesController {
+  @override
+  Future<SearchesState> build() async {
+    final searchesStreamData = await ref.watch(searchesProvider.future);
+    final List<SearchModel> allSearches = <SearchModel>[];
+    DocumentSnapshot? lastDoc;
+    searchesStreamData.map(allSearches.add);
+    final prevState = state.valueOrNull;
+    if (prevState == null) {
+      return SearchesState(
+        searchesList: searchesStreamData,
+        lastDocument: lastDoc,
+      );
+    }
+    final newList = prevState.searchesList;
+    for (final element in searchesStreamData.reversed) {
+      if (!newList.contains(element)) {
+        newList.insert(0, element);
+      }
+    }
+    return SearchesState(
+      searchesList: newList,
+    );
   }
 }
--- lib/src/features/core/weather/domain/searches_controller.g.dart
+++ lib/src/features/core/weather/domain/searches_controller.g.dart
@@ -20,5 +20,22 @@ final searchesProvider = StreamProvider<List<SearchModel>>.internal(
 );
 
 typedef SearchesRef = StreamProviderRef<List<SearchModel>>;
+String _$searchesControllerHash() =>
+    r'18ed5eca276a59e5a4f0cbfbebc7ef185492ff49';
+
+/// See also [SearchesController].
+@ProviderFor(SearchesController)
+final searchesControllerProvider =
+    AsyncNotifierProvider<SearchesController, SearchesState>.internal(
+  SearchesController.new,
+  name: r'searchesControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$searchesControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SearchesController = AsyncNotifier<SearchesState>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
